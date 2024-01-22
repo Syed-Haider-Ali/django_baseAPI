7f46920ba7df8d05b3f9941902c368564eeb05fd
Syed Haider Ali <s.haider0303@gmail.com>
2024-01-22 10:58:18 +0500

    Created Get API

--- utils/common_crud.py
+++ utils/common_crud.py
@@ -1,12 +1,16 @@
 from rest_framework.viewsets import ModelViewSet
 from utils.helper import create_response, get_first_error_message_from_serializer_errors, paginate_data
 from utils.response_messages import *
+from django.db.models import Q
 
 
 class BaseAPIView(ModelViewSet):
     serializer_class = None
     select_related_args = []
     prefetch_related_args = []
+    OR_filters = {}
+    AND_filters = {}
+    order_by = "-id"
 
     def create_record(self, request):
         try:
@@ -20,4 +24,28 @@ class BaseAPIView(ModelViewSet):
         except Exception as e:
             return create_response({str(e)}, UNSUCCESSFUL, 500)
 
+    def get_records(self, request):
+        try:
+            order = request.query_params.get('order', 'desc')
+            order_by = request.query_params.get('order_by', 'id')
+            if order and order_by:
+                order_by_ = order_by.lower()
+                if hasattr(self.serializer_class.Meta.model, order_by_) and order_by != "id":
+                    self.order_by = order_by_
+                if order:
+                    if order == 'desc':
+                        self.order_by = f"-{order_by_}"
+                    else:
+                        self.order_by = order_by_
 
+            data = self.serializer_class.Meta.model.objects.select_related(*self.select_related_args).prefetch_related(*self.prefetch_related_args).filter(Q(**self.OR_filters, _connector=Q.OR)).filter(**self.AND_filters).order_by(self.order_by)
+
+            data, count = paginate_data(data, request)
+            serialized_data = self.serializer_class(data, many=True).data
+            response_data = {
+                "count": count,
+                "data": serialized_data
+            }
+            return create_response(response_data, SUCCESSFUL, 200)
+        except Exception as e:
+            return create_response({'error': str(e)}, UNSUCCESSFUL, 500)
